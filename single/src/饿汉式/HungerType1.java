0d654a75edd0e95b2151f0cfcccd036043fe5479
ghdong366 <george366@163.com>
2022-11-15 00:58:50 +0800

    单例模式-饿汉式（静态变量方式）

---
+++ single/src/饿汉式/HungerType1.java
@@ -0,0 +1,35 @@
+package 饿汉式;
+
+/**
+ * @author Admin
+ * 饿汉式
+ * (静态变量的方式)
+ */
+public class HungerType1 {
+    public static void main(String[] args) {
+
+        //测试
+        CarSingleton instance = CarSingleton.getInstance();
+        CarSingleton instance2 = CarSingleton.getInstance();
+        System.out.println(instance==instance2);
+    }
+
+}
+
+/**
+ * 单例类
+ */
+class CarSingleton{
+
+    //本类内部创建对象实例
+    private final static CarSingleton instance = new CarSingleton();
+
+    // 构造器私有化，不能new
+    private CarSingleton(){
+    }
+
+    public static CarSingleton getInstance(){
+        return instance;
+    }
+
+}
