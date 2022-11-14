5c09c44e022faa17ddd4ac319ee626c95a0a7e84
ghdong366 <george366@163.com>
2022-11-15 01:28:09 +0800

    单例模式-懒汉式（线程不安全的方式）实际模拟

--- single/src/懒汉式/type1/LazyType1.java
+++ single/src/懒汉式/type1/LazyType1.java
@@ -7,9 +7,20 @@ package 懒汉式.type1;
  */
 public class LazyType1 {
     public static void main(String[] args) {
-        Moon instance1 = Moon.getInstance();
-        Moon instance2 = Moon.getInstance();
-        System.out.println(instance1 == instance2);
+        //Moon instance1 = Moon.getInstance();
+        //Moon instance2 = Moon.getInstance();
+        //System.out.println(instance1 == instance2); //这里结果为true
+
+        System.out.println("模拟多个线程创建单例对象");
+        //这里模拟多个线程创建单例对象
+        for (int i = 0; i < 20; i++) {
+            new Thread(()->{
+                String name = Thread.currentThread().getName();
+                Moon instance = Moon.getInstance();
+                System.out.println(name+"创建了Moon实例:"+instance.hashCode());
+            },"线程"+i).start();
+        }
+
     }
 }
 
